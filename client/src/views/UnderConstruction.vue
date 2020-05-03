0ec82a7df5a2ed5885c7c3c3bb9f15023213740c
DavidDual <daviddriessen@live.nl>
2020-05-03 17:27:31 +0200

    Better UnderConstruction page

--- client/src/views/UnderConstruction.vue
+++ client/src/views/UnderConstruction.vue
@@ -1,10 +1,8 @@
 <template>
   <v-container style="margin-top: 100px;">
     <v-flex class="d-flex justify-center">
-      <v-card width="400">
-        <v-card-title>
-          Under construction
-        </v-card-title>
+      <v-card width="800">
+        <v-img src="../assets/underconstruct.png" />
       </v-card>
     </v-flex>
   </v-container>
