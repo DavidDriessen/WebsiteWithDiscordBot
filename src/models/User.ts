2a072981548d2b813760f3477d8c51753f0a6435
DavidDual <daviddriessen@live.nl>
2020-06-07 19:10:43 +0200

    Update email if empty Added streamer to notification

--- src/models/User.ts
+++ src/models/User.ts
@@ -3,54 +3,61 @@ import Attendee from './Attendee';
 import Event from './Event';
 
 interface IUser {
-    id: string;
-    avatar: string | null | undefined;
-    username: string;
-    discriminator: string;
+  id: string;
+  avatar: string | null | undefined;
+  username: string;
+  discriminator: string;
 
-    bot?: boolean;
-    email?: string;
-    flags?: number;
-    locale?: string;
-    verified?: boolean;
-    mfa_enabled?: string;
-    premium_type?: number;
+  bot?: boolean;
+  email?: string;
+  flags?: number;
+  locale?: string;
+  verified?: boolean;
+  mfa_enabled?: string;
+  premium_type?: number;
 }
 
 @Table
 export class User extends Model<User> {
 
-    @Column
-    public name!: string;
+  @Column
+  public name!: string;
 
-    @Column
-    public email!: string;
+  @Column
+  public email!: string;
 
-    @Column
-    public avatar!: string;
+  @Column
+  public avatar!: string;
 
-    @Column
-    public discordId!: string;
+  @Column
+  public discordId!: string;
 
-    @Column
-    public role!: string;
+  @Column
+  public role!: string;
 
-    @HasMany(() => Event, 'streamerId')
-    public streams!: Event[];
+  @HasMany(() => Event, 'streamerId')
+  public streams!: Event[];
 
-    @BelongsToMany(() => Event, () => Attendee)
-    public events!: Event[];
+  @BelongsToMany(() => Event, () => Attendee)
+  public events!: Event[];
 
-    public static get(discordUser: IUser) {
-        return User.findOrCreate({
-            where: {discordId: discordUser.id}, defaults: {
-                name: discordUser.username,
-                email: discordUser.email,
-                avatar: 'https://cdn.discordapp.com/avatars/' + discordUser.id +
-                    '/' + discordUser.avatar + '.jpg',
-            },
-        });
+  public static async get(discordUser: IUser) {
+    const res = await User.findOrCreate({
+      where: {discordId: discordUser.id}, defaults: {
+        name: discordUser.username,
+        email: discordUser.email,
+        avatar: 'https://cdn.discordapp.com/avatars/' + discordUser.id +
+          '/' + discordUser.avatar + '.jpg',
+      },
+    });
+
+    if (!res[0].email && discordUser.email) {
+      res[0].email = discordUser.email;
+      res[0].save();
     }
+
+    return res;
+  }
 }
 
 export default User;
--- src/workers/EventWorker.ts
+++ src/workers/EventWorker.ts
@@ -11,77 +11,77 @@ import {EventDiscord} from '../discord/EventDiscord';
 @CronController('jobs')
 export class EventWorker {
 
-    private static getChannel() {
-        return client.channels.get(discordConfig.channel.notify) as TextChannel;
-    }
+  private static getChannel() {
+    return client.channels.get(discordConfig.channel.notify) as TextChannel;
+  }
 
-    private static getAttendees(attendees: User[], decision: number) {
-        if (attendees.length === 0) {
-            return '';
-        }
-        const filteredDiscord = attendees.filter((user) => user.discordId);
-        if (filteredDiscord.length === 0) {
-            return '';
-        }
-        const filteredDecision = filteredDiscord
-            // @ts-ignore
-            .filter((user) => user.Attendee.decision === decision);
-        if (filteredDecision.length === 0) {
-            return '';
-        }
-        const parsed = filteredDecision.map((user) => '<@' + user.discordId + '>').join(', ');
-        if (decision === 1) {
-            return '\nAttending: ' + parsed;
-        }
-        if (decision === 2) {
-            return '\nUndecided: ' + parsed;
-        }
-        return '';
+  private static getAttendees(attendees: User[], decision: number) {
+    if (attendees.length === 0) {
+      return '';
     }
-
-    @Cron('eventReminder', '0 */10 * * * *')
-    public async eventReminder() {
-        const now = moment();
-        const nowPlus = now.clone().add(35, 'minutes');
-        const nowMinus = now.clone().add(25, 'minutes');
-        const events = await Event.findAll({
-            where: {start: {[Op.gt]: nowMinus, [Op.lte]: nowPlus}},
-            include: ['series', 'streamer', 'attendees'],
-        });
-        for (const event of events) {
-            EventWorker.sendNotification(event).then();
-        }
+    const filteredDiscord = attendees.filter((user) => user.discordId);
+    if (filteredDiscord.length === 0) {
+      return '';
     }
-
-    @Cron('updateEventChannel', '0 0 0 * * *')
-    public updateEventChannel() {
-        EventDiscord.updateChannel().then();
+    const filteredDecision = filteredDiscord
+      // @ts-ignore
+      .filter((user) => user.Attendee.decision === decision);
+    if (filteredDecision.length === 0) {
+      return '';
     }
+    const parsed = filteredDecision.map((user) => '<@' + user.discordId + '>').join(', ');
+    if (decision === 1) {
+      return '\nAttending: ' + parsed;
+    }
+    if (decision === 2) {
+      return '\nUndecided: ' + parsed;
+    }
+    return '';
+  }
 
-    public static async sendNotification(event: Event) {
-        const attendees = await event.$get('attendees');
-        EventWorker.getChannel()
-            .send('Reminder: ' + event.title +
-                ' will be starting **' +
-                (moment().isAfter(event.start) ? 'Soon' : moment(event.start).fromNow()) + '**.' +
-                EventWorker.getAttendees(attendees, 1) +
-                EventWorker.getAttendees(attendees, 2) +
-                (event.roomcode ? '\nRoom code: ' + event.roomcode : ''))
-            .then(() => {
-                if (event.roomcode && event.messageID) {
-                    this.getChannel().fetchMessages()
-                        .then((msgs) => {
-                            if (event.messageID) {
-                                const msg = msgs.get(event.messageID);
-                                if (msg) {
-                                    msg.delete().then(() => {
-                                        event.messageID = '';
-                                        event.save({fields: ['messageID']});
-                                    });
-                                }
-                            }
-                        });
+  @Cron('eventReminder', '0 */10 * * * *')
+  public async eventReminder() {
+    const now = moment();
+    const nowPlus = now.clone().add(35, 'minutes');
+    const nowMinus = now.clone().add(25, 'minutes');
+    const events = await Event.findAll({
+      where: {start: {[Op.gt]: nowMinus, [Op.lte]: nowPlus}},
+      include: ['series', 'streamer', 'attendees'],
+    });
+    for (const event of events) {
+      EventWorker.sendNotification(event).then();
+    }
+  }
+
+  @Cron('updateEventChannel', '0 0 0 * * *')
+  public updateEventChannel() {
+    EventDiscord.updateChannel().then();
+  }
+
+  public static async sendNotification(event: Event) {
+    const attendees = await event.$get('attendees');
+    EventWorker.getChannel()
+      .send('Reminder: ' + event.title + ' will be starting **' +
+        (moment().isAfter(event.start) ? 'Soon' : moment(event.start).fromNow()) + '**.' +
+        '\nStreamer: <@' + event.streamer.discordId + '>' +
+        EventWorker.getAttendees(attendees, 1) +
+        EventWorker.getAttendees(attendees, 2) +
+        (event.roomcode ? '\nRoom code: ' + event.roomcode : ''))
+      .then(() => {
+        if (event.roomcode && event.messageID) {
+          this.getChannel().fetchMessages()
+            .then((msgs) => {
+              if (event.messageID) {
+                const msg = msgs.get(event.messageID);
+                if (msg) {
+                  msg.delete().then(() => {
+                    event.messageID = '';
+                    event.save({fields: ['messageID']});
+                  });
                 }
+              }
             });
-    }
+        }
+      });
+  }
 }
