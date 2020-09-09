ebeb081f1f9cd4cb96ccd5a2e47714348451c539
DavidDual <daviddriessen@live.nl>
2020-09-09 15:02:51 +0200

    Poll result chart improved

--- client/src/components/AnimeSelector/Chart.vue
+++ client/src/components/AnimeSelector/Chart.vue
@@ -14,16 +14,61 @@ export default {
     }
   },
   mounted() {
-    this.renderChart({
-      labels: this.labels,
-      datasets: [
-        {
-          label: "Data One",
-          backgroundColor: "#f87979",
-          data: this.values
-        }
-      ]
-    });
+    this.renderChart(
+      {
+        labels: this.labels,
+        datasets: [
+          {
+            label: "Data One",
+            backgroundColor: "#f87979",
+            data: this.values
+          }
+        ]
+      },
+      {
+        scales: {
+          yAxes: [
+            {
+              ticks: {
+                beginAtZero: true
+              },
+              gridLines: {
+                display: true
+              }
+            }
+          ],
+          xAxes: [
+            {
+              ticks: {
+                beginAtZero: true
+              },
+              gridLines: {
+                display: false
+              }
+            }
+          ]
+        },
+        legend: {
+          display: false
+        },
+        tooltips: {
+          enabled: true,
+          mode: "single",
+          callbacks: {
+            label: function(tooltipItems) {
+              return (
+                tooltipItems.yLabel +
+                " member" +
+                (tooltipItems.yLabel > 1 ? "s" : "")
+              );
+            }
+          }
+        },
+        responsive: true,
+        maintainAspectRatio: true,
+        height: 200
+      }
+    );
   }
 };
 </script>
