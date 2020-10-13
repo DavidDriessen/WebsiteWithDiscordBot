44a65fe25bfe9623295cbaeff1d2e99892912eec
david <daviddriessen@live.nl>
2020-10-13 14:33:32 +0200

    Migration bug fixed

--- src/database/migrations/20200512074311-edit-poll-votes.js
+++ src/database/migrations/20200512074311-edit-poll-votes.js
@@ -2,32 +2,32 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
     return Promise.all([
-      queryInterface.renameColumn('PollVotes', 'user', 'ballot'),
-      queryInterface.changeColumn('PollVotes', 'ballot', {
-        type: Sequelize.INTEGER,
-        allowNull: false,
-        references: {
-          model: "Ballots",
-          key: "id",
-        },
-        onUpdate: "cascade",
-        onDelete: "cascade",
-      }),
+      queryInterface.renameColumn('PollVotes', 'user', 'ballot')
+        .then(() => queryInterface.changeColumn('PollVotes', 'ballot', {
+          type: Sequelize.INTEGER,
+          allowNull: false,
+          references: {
+            model: "Ballots",
+            key: "id",
+          },
+          onUpdate: "cascade",
+          onDelete: "cascade",
+        })),
       queryInterface.addColumn('PollVotes', 'choice', {
         type: Sequelize.INTEGER
-      })
-    ]);
+      })])
   },
   down: (queryInterface, Sequelize) => {
     return Promise.all([
-      queryInterface.renameColumn('PollVotes', 'ballot', 'user').then(() =>
-        queryInterface.changeColumn('PollVotes', 'user', {
-          type: Sequelize.INTEGER,
-          references: {
-            model: "Users",
-            key: "id",
-          }
-        })),
+      queryInterface.renameColumn('PollVotes', 'ballot', 'user')
+        .then(() =>
+          queryInterface.changeColumn('PollVotes', 'user', {
+            type: Sequelize.INTEGER,
+            references: {
+              model: "Users",
+              key: "id",
+            }
+          })),
       queryInterface.removeColumn('PollVotes', 'choice')
     ]);
   }
