abbf7e16fb23293b26dca08fce1999dc49b4cd98
david <daviddriessen@live.nl>
2020-10-16 17:01:58 +0200

    Changed discord command guard to check for db role

--- src/discord/Guards.ts
+++ src/discord/Guards.ts
@@ -1,15 +1,29 @@
 import {GuardFunction} from '@typeit/discord';
-import {Role} from 'discord.js';
+import User from '../database/models/User';
+
+
+export function AllowDM() {
+  const check: GuardFunction<'commandMessage'> =
+    async ([message]: any, client: any, next: () => any, guardDatas: any) => {
+      if (message.channel.type === 'dm') {
+        guardDatas.DM = true;
+      }
+      await next();
+    };
+  return check;
+}
 
 export function CheckRole(role: string) {
   const check: GuardFunction<'commandMessage'> =
-    async ([message]: any, client: any, next: () => any) => {
-      if (message.guild) {
-        const roleOb = message.guild.roles.cache.find((r: Role) => r.name === role);
-        if (roleOb && message.member) {
-          if (message.member.roles.cache.has(roleOb.id)) {
-            await next();
-          }
+    async ([message]: any, client: any, next: () => any, guardDatas: any) => {
+      if (message.guild && !message.author.bot) {
+        const user = await User.get(message.author);
+        if (user[0].role === role) {
+          await next();
+        }
+      } else {
+        if (guardDatas.DM) {
+          await next();
         }
       }
     };
